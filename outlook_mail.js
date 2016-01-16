7a64b0b465b75b32a4522fc23bc6b2c1a7bcf317
Meir Bank <globleedev@gmail.com>
2016-01-15 22:24:39 -0500

    done
    done

---
+++ outlook_mail.js
@@ -0,0 +1,49 @@
+
+var request = require("request")
+module.exports=function(mysubject, message, sendto,  token, callback){
+
+	var options = {
+		url:"https://outlook.office.com/api/v2.0/me/sendmail",
+		json:{
+			Message: {
+			    Subject: mysubject,
+			    Body: {
+			      ContentType: "Text",
+			      Content: message
+			    },
+			    ToRecipients: [
+			    ]
+			  },
+			  "SaveToSentItems": "true"
+		},
+		headers:{
+			Authorization:"bearer "+token,
+			Accept: 'text/*, application/xml, application/json; odata.metadata=none',
+		}
+	};
+	options.headers['Content-type'] = 'application/json; odata.metadata=none'
+	for (var i=0; i< sendto.length; i++){
+		options.json.Message.ToRecipients.push({  EmailAddress: { Address:sendto[i] }   })
+	}
+	console.log(options, options.json.Message.ToRecipients)
+	request.post(options, function(err, res, body){
+		//console.log(res.statusCode, body, err)
+		if (err){
+			return callback(err)
+		} else {
+
+			try{
+				body = JSON.parse(body);
+				if (body.error){
+					return callback(body.error)
+				} else {
+					return callback(null)
+				}
+			} catch (ex){
+				console.log(ex)
+				return callback(ex)
+			}
+		}
+	});
+
+};
